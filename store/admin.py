96bb71753900a0fc15c51b76b531620551284bed
omkar130 <72204874+omkar130@users.noreply.github.com>
2025-02-23 19:39:04 +0000

    second commit

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('product_name',)}
 
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Variation)
