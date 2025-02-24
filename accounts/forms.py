6987d4201c3a0a43a78da13452e1cef6ba3bfb5c
omkar130 <72204874+omkar130@users.noreply.github.com>
2025-02-24 22:21:31 +0000

    third commit

---
+++ accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm Password'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name','last_name','phone_number','email','password']
+
+    def __init__(self,*args,**kwrgs):
+        super(RegistrationForm,self).__init__(*args,**kwrgs)
+        self.fields['first_name'].widget.attrs['placeholder']='Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder']='Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder']='Enter Email'
+        self.fields['phone_number'].widget.attrs['placeholder']='Enter Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'        #this one to apply to all the fields so that 'form-control' class styling will get apply to all fields in html
+
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm,self).clean()                 # calls the clean method of RegistrationForm parent
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password Does Not Match"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+def register(request):
+    if request.method == 'POST':
+        form =  RegistrationForm(request.POST)       #getting data entered by user
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user=Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),                #encoding user's primary key
+                'token':default_token_generator.make_token(user),                 #to generate token
+            })
+
+            to_email = email
+            send_mail = EmailMessage(mail_subject,message,to=[to_email])
+            send_mail.send()
+
+            return redirect('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
+    context ={
+        "form":form,
+    }
+    return render(request,"accounts/register.html",context)
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user= auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Invalid Login Credentials')
+            return redirect('login')
+    return render(request,'accounts/login.html')
+
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request,'You are logged out')
+    return redirect('login')
+
+
+def activate(request,uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()     #decoding users primary key
+        user = Account._default_manager.get(pk=uid)      #with primary key getting user
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        message.success(request,'Congratulations!, Your account is activated')
+        return redirect('login')
+    else:
+        message.error(request,'Invalid activation link')
+        return redirect('register')
+
+def dashboard(request):
+    return render(request,"accounts/dashboard.html")
